eda2db42f56ed79dedef3039fc46aca7524297c4
juliansini bananini <mendozsjulio01@gmail.com>
2025-12-02 11:07:02 -0600

    Add files via upload

---
+++ Pracrica-3/Pracrica-3/Pracrica-3/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Pracrica_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.botonComenzar = new System.Windows.Forms.Button();
+            this.Salir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(91, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(363, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sensor Luvia FC-37 o YL-83";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(36, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(219, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Estado del Sensor";
+            // 
+            // botonComenzar
+            // 
+            this.botonComenzar.BackColor = System.Drawing.Color.Red;
+            this.botonComenzar.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonComenzar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.botonComenzar.Location = new System.Drawing.Point(85, 292);
+            this.botonComenzar.Name = "botonComenzar";
+            this.botonComenzar.Size = new System.Drawing.Size(110, 31);
+            this.botonComenzar.TabIndex = 2;
+            this.botonComenzar.Text = "Comenzar";
+            this.botonComenzar.UseVisualStyleBackColor = false;
+            // 
+            // Salir
+            // 
+            this.Salir.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.Salir.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Salir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Salir.Location = new System.Drawing.Point(97, 356);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(75, 32);
+            this.Salir.TabIndex = 3;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(375, 215);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 140);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ClientSize = new System.Drawing.Size(615, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.botonComenzar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button botonComenzar;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Pracrica-3/Pracrica-3/Pracrica-3/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pracrica_3
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        private bool isBlinking;
+        private SoundPlayer soundPlayer;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort = new SerialPort("COM3", 9600);
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            botonComenzar.Click += botonComenzar_Click;
+            timer1.Interval = 500;// Intervalo de parpadeo en milisegundos
+            timer1.Tick += timer1_Tick;
+            soundPlayer = new SoundPlayer("C:\\Users\\Usuario1\\OneDrive\\Escritorio\\Underwater Water Ambience 04.wav");
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+            base.OnFormClosing(e);
+        }
+        /* private void Timer1_Tick(object? sender, EventArgs e)
+         {
+             throw new NotImplementedException();
+         }*/
+
+        private void botonComenzar_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                serialPort.Open();
+            }
+
+        }
+
+        void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine();
+            this.Invoke(new Action(() => {
+                label1.Text = "Sensor Status: " + (data.Trim() == "1" ? "Water Detected" : "No Water Detected");
+                if (data.Trim() == "1")
+                {
+                    if (!isBlinking)
+                    {
+                        timer1.Start();
+                        soundPlayer.PlayLooping();
+                        pictureBox1.BackColor = System.Drawing.Color.Red;
+                        isBlinking = true;
+                    }
+                }
+                else
+                {
+                    if (isBlinking)
+                    {
+                        soundPlayer.Stop();
+                        pictureBox1.BackColor = System.Drawing.Color.Gray;
+                        isBlinking = false;
+                    }
+                }
+            }));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = pictureBox1.BackColor == System.Drawing.Color.Gray ? System.Drawing.Color.Yellow : System.Drawing.Color.Gray;
+        }
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            serialPort.Close();
+            Application.Exit();
+        }
+    }
+
+}
+
